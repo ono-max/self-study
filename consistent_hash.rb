4da5d5b2248a18e664b0c9193ee62935805d1b14
Naoto Ono <onoto1998@gmail.com>
2026-06-28 20:40:09 +0900

    Add functionality for unique node tracking and scaling in consistent hashing

--- consistent_hash.rb
+++ consistent_hash.rb
@@ -74,3 +74,52 @@ migrated_count = 0
 }
 
 puts migrated_count
+
+# Coding the Trade-off.
+
+ch = ConsistentHash.new
+ch.add_node("Server A")
+ch.add_node("Server B")
+ch.add_node("Server C")
+
+unique_nodes = Set.new
+100.times do |i|
+    key = "user_1_#{i}"
+    node_name = ch.get_node(key)[:node]
+    unique_nodes.add(node_name)
+end
+
+puts unique_nodes.size
+
+# Scaling Up!
+
+ch = ConsistentHash.new
+ch.add_node("Server A")
+ch.add_node("Server B")
+ch.add_node("Server C")
+
+initial_mapping = {}
+
+10000.times {|i|
+    key = "user_#{i}"
+    node_name = ch.get_node(key)[:node]
+    initial_mapping[key] = node_name
+}
+
+ch.add_node("Server D")
+
+original_node_2_count = {}
+
+10000.times {|i|
+    key = "user_#{i}"
+    new_node_name = ch.get_node(key)[:node]
+    original_node = initial_mapping[key]
+    if original_node != new_node_name
+        if original_node_2_count.key?(original_node)
+            original_node_2_count[original_node] += 1
+        else
+            original_node_2_count[original_node] = 1
+        end
+    end
+}
+puts original_node_2_count
