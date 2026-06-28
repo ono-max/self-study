84bbf78328fef0353c9f64e64dcdf3321b391369
Naoto Ono <onoto1998@gmail.com>
2026-06-28 17:35:25 +0900

    Implement consistent hashing with node addition, retrieval, and removal

---
+++ consistent_hash.rb
@@ -0,0 +1,76 @@
+require 'digest'
+
+class ConsistentHash
+    def initialize
+        @ring = []
+    end
+
+    def hash(key)
+        Digest::MD5.hexdigest(key).to_i(16)
+    end
+
+    def add_node(node_name, replicas = 100)
+        replicas.times {|i|
+            # [1, 5, 7, 8]
+            # 0 => 0
+            # 6 => 2
+            # target < node.hash_val
+            calculated_hash = hash("#{node_name}-#{i}")
+            idx = 0
+            @ring.each {|node|
+                if calculated_hash < node[:hash_val]
+                    break
+                end
+                idx += 1
+            }
+            @ring.insert(idx, {
+                hash_val: calculated_hash,
+                node: node_name
+            })
+        }
+    end
+
+    def get_node(key)
+        calculated_hash = hash(key)
+        idx = 0
+        @ring.each{|node|
+            if calculated_hash <= node[:hash_val]
+                return node
+            end
+        }
+        @ring.first
+    end
+
+    def remove_node(node_name)
+        @ring.reject! {|node| node[:node] == node_name}
+    end
+end
+
+node_2_counts = {}
+
+initial_mapping = {}
+
+ch = ConsistentHash.new
+ch.add_node("Server A")
+ch.add_node("Server B")
+ch.add_node("Server C")
+
+10000.times {|i|
+    key = "user_#{i}"
+    node_name = ch.get_node(key)[:node]
+    initial_mapping[key] = node_name
+}
+
+ch.remove_node("Server B")
+
+migrated_count = 0
+
+10000.times {|i|
+    key = "user_#{i}"
+    node_name = ch.get_node(key)[:node]
+    if initial_mapping[key] != node_name
+        migrated_count += 1
+    end
+}
+
+puts migrated_count
